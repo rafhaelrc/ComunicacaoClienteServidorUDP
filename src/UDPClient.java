c6a916cf72f7f23b4c694b6c3c65a554b81c8a5d
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-20 18:15:26 -0300

    alterando

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.net.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -17,39 +18,38 @@ class UDPClient {
 
         //Cria Stream de Enrada
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-        //Cria socket Cliente
+        System.out.println("Digite quantos pacotes serão enviados."); 
+        String quantidade = inFromUser.readLine();
+        int numero = Integer.parseInt(quantidade);
+        ArrayList<String> temposInicial = new ArrayList<String>();
+        ArrayList<String> temposFinais = new ArrayList<String>();
+         //Cria socket Cliente
         DatagramSocket clientSocket = new DatagramSocket();
-
         //Traduz HostName para Endereço IP Usando DNS
         InetAddress IPAddress = InetAddress.getByName("localhost");
-
-        //Testezinho pra ver se ta pegando o ip certo
+       //Testezinho pra ver se ta pegando o ip certo
         System.out.println("IPADRESS: " + IPAddress.getHostAddress());
 
-
+       for(int i = 0; i<numero;i++){
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
-        String sentence = inFromUser.readLine();
+        //String sentence = inFromUser.readLine();
+        
+        String sentence = "Teste";
         sendData = sentence.getBytes();
 
-
-        /*
-         * DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss,SSS"); Date
-         * date = new Date();
-         */
         Calendar c = new GregorianCalendar();
 
-        //Pega a hora em milisegundos
-        c.setTimeInMillis(System.currentTimeMillis());
-        System.out.println("Pegando o tempo em MiliSegundos: " + c.getTimeInMillis());
-
+       
         // Cria datagrama com dados a enviar, dados a receber, tamanho, endereço IP e Porta
         DatagramPacket sendPacket =
                 new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
         // Enviando um datagrama para o servidor
         clientSocket.send(sendPacket);
+         //Pega a hora em milisegundos
+        c.setTimeInMillis(System.currentTimeMillis());
+        System.out.print("hora de envio: " + c.getTimeInMillis()+ " ");
 
         //System.out.println("Enviou horas: " + dateFormat.format(date));
 
@@ -58,13 +58,14 @@ class UDPClient {
                 new DatagramPacket(receiveData, receiveData.length);
         // lê datagrama do servidor
         clientSocket.receive(receivePacket);
+        //Pega a hora em milisegundos
+        c.setTimeInMillis(System.currentTimeMillis());
+        System.out.print("hora de chegada: " + c.getTimeInMillis()+ " ");
 
-        //System.out.println("Recebeu horas: " + dateFormat.format(date));
-
-
-        String modifiedSentence =
-                new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence);
-        clientSocket.close();
+        }
+//        String modifiedSentence =
+//                new String(receivePacket.getData());
+//        System.out.println("FROM SERVER:" + modifiedSentence);
+       clientSocket.close();
     }
 }
