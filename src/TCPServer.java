beef217e86d8cc32cfc7543da018545668edbe3f
Rafhael Cunha <rcrafhaelrc@gmail.com>
2012-05-20 16:52:42 -0300

    Comentários Classes

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -12,24 +12,38 @@ import java.util.Date;
 class TCPServer {
 
     public static void main(String args[]) throws Exception {
+
+        //Cria um datagrama socket na parta 8080
         DatagramSocket serverSocket = new DatagramSocket(8080);
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
 
         while (true) {
+
+            //Cria espaço para datagramas recebidos
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            //Recebe datagrama
             serverSocket.receive(receivePacket);
+
             String sentence = new String(receivePacket.getData());
             System.out.println("Sentença: " + sentence);
+            
+            //Obtem endereço IP
             InetAddress IPAddress = receivePacket.getAddress();
+            //Obtem número da porta
             int port = receivePacket.getPort();
+            
+            // Deixa a sentença em letras maiusculas.
             String capitalizedSentence = sentence.toUpperCase();
             sendData = capitalizedSentence.getBytes();
+            
+            //Cria datagrama para enviar para o Cliente
             DatagramPacket sendPacket =
                     new DatagramPacket(sendData, sendData.length, IPAddress,
                     port);
+            //Escreve o Datagrama no Socket
             serverSocket.send(sendPacket);
-            
+
         }
     }
 }
--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,8 +1,7 @@
 
 /**
  *
- * @author RafhaelRC
- * Classe que implementa o Cliente UDP
+ * @author RafhaelRC Classe que implementa o Cliente UDP
  */
 import java.io.*;
 import java.net.*;
@@ -15,52 +14,54 @@ import java.util.GregorianCalendar;
 class UDPClient {
 
     public static void main(String args[]) throws Exception {
-        
+
         //Cria Stream de Enrada
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        
+
         //Cria socket Cliente
         DatagramSocket clientSocket = new DatagramSocket();
-        
+
         //Traduz HostName para Endereço IP Usando DNS
         InetAddress IPAddress = InetAddress.getByName("localhost");
-        
+
         //Testezinho pra ver se ta pegando o ip certo
         System.out.println("IPADRESS: " + IPAddress.getHostAddress());
-        
-        
+
+
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
         String sentence = inFromUser.readLine();
         sendData = sentence.getBytes();
-        
-        
-        /*DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss,SSS");
-        Date date = new Date();*/
-        Calendar c = new GregorianCalendar();  
-        
+
+
+        /*
+         * DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss,SSS"); Date
+         * date = new Date();
+         */
+        Calendar c = new GregorianCalendar();
+
         //Pega a hora em milisegundos
         c.setTimeInMillis(System.currentTimeMillis());
-        System.out.println("Pegando o tempo em MiliSegundos: "+ c.getTimeInMillis());
-        
+        System.out.println("Pegando o tempo em MiliSegundos: " + c.getTimeInMillis());
+
         // Cria datagrama com dados a enviar, dados a receber, tamanho, endereço IP e Porta
         DatagramPacket sendPacket =
                 new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-        
+
         // Enviando um datagrama para o servidor
         clientSocket.send(sendPacket);
-        
+
         //System.out.println("Enviou horas: " + dateFormat.format(date));
-        
+
         // Cria um receveiPacket e lê datagrama do servidor
         DatagramPacket receivePacket =
                 new DatagramPacket(receiveData, receiveData.length);
         // lê datagrama do servidor
         clientSocket.receive(receivePacket);
-        
+
         //System.out.println("Recebeu horas: " + dateFormat.format(date));
-        
-        
+
+
         String modifiedSentence =
                 new String(receivePacket.getData());
         System.out.println("FROM SERVER:" + modifiedSentence);
