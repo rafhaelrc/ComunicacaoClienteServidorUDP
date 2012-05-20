c8d1ba67174ea75fdb94ea6a5346d1edd05094ba
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-20 20:04:56 -0300

    rtt

---
+++ src/RTT.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class RTT {
+    
+    ArrayList<String> temposIniciais;
+    ArrayList<String> temposFinais;
+    long rtt =0;
+
+    public RTT(ArrayList<String> temposIniciais, ArrayList<String> temposFinais) {
+        this.temposIniciais = temposIniciais;
+        this.temposFinais = temposFinais;
+    }
+
+    public ArrayList<String> getTemposFinais() {
+        return temposFinais;
+    }
+
+    public ArrayList<String> getTemposIniciais() {
+        return temposIniciais;
+    }
+
+    public long calculaRTT(){
+        for (int i = 0; i < temposIniciais.size() ; i++) {
+        long min = Long.parseLong(temposFinais.get(i)) - Long.parseLong(temposIniciais.get(i));  
+        if(min<rtt){
+           rtt = min;
+         }  
+            
+        }
+    
+    return rtt;
+    }
+}
