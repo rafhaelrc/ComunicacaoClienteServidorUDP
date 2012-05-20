956db39be761a8f730f0daf348f9fc135bac2d58
Marcelo Lopes <marcelomaialopes@yahoo.com.br>
2012-05-20 18:28:36 -0300

    criando UDP

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -16,21 +16,25 @@ class UDPClient {
 
     public static void main(String args[]) throws Exception {
 
+        ArrayList<String> tempoSaida = new ArrayList<String>();
+        ArrayList<String> tempoRetorno = new ArrayList<String>();
         //Cria Stream de Enrada
+        Tempo tempo = new Tempo();
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Digite quantos pacotes serão enviados."); 
         String quantidade = inFromUser.readLine();
         int numero = Integer.parseInt(quantidade);
-        ArrayList<String> temposInicial = new ArrayList<String>();
-        ArrayList<String> temposFinais = new ArrayList<String>();
-         //Cria socket Cliente
+        //Cria socket Cliente
         DatagramSocket clientSocket = new DatagramSocket();
         //Traduz HostName para Endereço IP Usando DNS
         InetAddress IPAddress = InetAddress.getByName("localhost");
        //Testezinho pra ver se ta pegando o ip certo
         System.out.println("IPADRESS: " + IPAddress.getHostAddress());
-
+  
        for(int i = 0; i<numero;i++){
+       
+       
+      
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
         //String sentence = inFromUser.readLine();
@@ -43,13 +47,17 @@ class UDPClient {
        
         // Cria datagrama com dados a enviar, dados a receber, tamanho, endereço IP e Porta
         DatagramPacket sendPacket =
-                new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                new DatagramPacket(sendData, sendData.length, IPAddress, 8080);
 
         // Enviando um datagrama para o servidor
         clientSocket.send(sendPacket);
          //Pega a hora em milisegundos
         c.setTimeInMillis(System.currentTimeMillis());
+        
         System.out.print("hora de envio: " + c.getTimeInMillis()+ " ");
+        tempoSaida.add(i, c.getTimeInMillis()+"");
+        
+        
 
         //System.out.println("Enviou horas: " + dateFormat.format(date));
 
@@ -61,11 +69,11 @@ class UDPClient {
         //Pega a hora em milisegundos
         c.setTimeInMillis(System.currentTimeMillis());
         System.out.print("hora de chegada: " + c.getTimeInMillis()+ " ");
-
+        tempoRetorno.add(i, c.getTimeInMillis()+"");
+        String modifiedSentence = new String(receivePacket.getData());
         }
-//        String modifiedSentence =
-//                new String(receivePacket.getData());
-//        System.out.println("FROM SERVER:" + modifiedSentence);
-       clientSocket.close();
+
+     clientSocket.close();
+       
     }
 }
